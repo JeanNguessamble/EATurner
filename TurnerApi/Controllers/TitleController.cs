9185faf5a6499756d87b83b8793ee72ffa0152b2
Jean N'Guessamble <bi90567@yahoo.com>
2016-09-01 01:28:55 -0400

    Developer Challenge

---
+++ TurnerApi/Controllers/TitleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TurnerData;
+
+namespace TurnerApi.Controllers
+{
+    public class TitleController : ApiController
+    {
+        public IEnumerable<Entity.title> Get()
+        {
+            return new TurnerData.Title { }.GetList(null);
+        }
+        public IEnumerable<dynamic> Get(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return null;
+
+            int id = ConvertToInt(value);
+            if (id > 0) return new TurnerData.Title { }.GetDetail(id);
+            else return new TurnerData.Title { }.GetList(value);
+        }
+        public IEnumerable<Entity.title> Search(string value)
+        {
+            if (string.IsNullOrEmpty(value)) return null;
+            return new TurnerData.Title { }.GetList(value);
+        }
+        int ConvertToInt(string value)
+        {
+            int rValue = 0;
+            try { rValue = Convert.ToInt32(value); }
+            catch { }
+            return rValue;
+        }
+    }
+}
---
+++ TurnerData/Entity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurnerData
+{
+    [Serializable]
+    public class Entity
+    {
+        public class title
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string sortable { get; set; }
+            public int? typeId { get; set; }
+            public int? releaseYear { get; set; }
+            internal DateTime? processedDateTime { get; set; }
+            public string processedDate { get { return processedDateTime == null || !processedDateTime.HasValue ? null : processedDateTime.Value.ToShortDateString(); } }
+            public string processedTime { get { return processedDateTime == null || !processedDateTime.HasValue ? null : processedDateTime.Value.ToShortTimeString(); } }
+        }
+        public class genre
+        {
+            public string name { get; set; }
+        }
+        public class participiant
+        {
+            public string name { get; set; }
+            public string role { get; set; }
+        }
+        public class award
+        {
+            public int? year { get; set; }
+            public string name { get; set; }
+        }
+        public class otherName
+        {
+            public string name { get; set; }
+            public string language { get; set; }
+        }
+        public class detail
+        {
+            public title movie { get; set; }
+            public string genre { get; set; }
+            public string description { get; set; }
+            public IEnumerable<award> awards { get; set; }
+            public IEnumerable<otherName> otherNames { get; set; }
+            public IEnumerable<participiant> participiants { get; set; }
+        }
+    }
+}
